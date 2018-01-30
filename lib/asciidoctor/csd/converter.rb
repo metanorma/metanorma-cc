961e0296cd078ced7d637ed8b74aa3e9c34a110c
Nick Nicholas <opoudjis@optusnet.com.au>
2018-01-30 22:56:01 +1100

    complete inheritance from ISO

--- lib/asciidoctor/csd/converter.rb
+++ lib/asciidoctor/csd/converter.rb
@@ -1,16 +1,6 @@
 require "asciidoctor"
 require "asciidoctor/csd/version"
-require "asciidoctor/iso"
-#require "asciidoctor/iso/isoxml/base"
-#require "asciidoctor/iso/isoxml/front"
-#require "asciidoctor/iso/isoxml/lists"
-#require "asciidoctor/iso/isoxml/inline_anchor"
-#require "asciidoctor/iso/isoxml/blocks"
-#require "asciidoctor/iso/isoxml/section"
-#require "asciidoctor/iso/isoxml/table"
-#require "asciidoctor/iso/isoxml/validate"
-#require "asciidoctor/iso/isoxml/utils"
-#require "asciidoctor/iso/isoxml/cleanup"
+require "asciidoctor/iso/converter"
 
 module Asciidoctor
   module Csd
@@ -20,7 +10,57 @@ module Asciidoctor
 
       register_for "csd"
 
-      alias_method :inline_image, :skip
+      def metadata_author(node, xml)
+        xml.creator **{ role: "author" } do |a|
+          a.technical_committee node.attr("technical-committee"),
+            **attr_code(type: node.attr("technical-committee-type"))
+        end
+      end
+
+      def title(node, xml)
+        ["en"].each do |lang|
+          xml.title **{ language: lang } do |t|
+            t << node.attr("title") 
+          end
+        end
+      end
+
+      def metadata_status(node, xml)
+        xml.status { |s| s << node.attr("status") }
+      end
+
+      def metadata_id(node, xml)
+        xml.id { |i| i << node.attr("docnumber") }
+      end
+
+      def metadata_copyright(node, xml)
+        from = node.attr("copyright-year") || Date.today.year
+        xml.copyright do |c|
+          c.from from
+          c.owner do |o|
+            o.affiliation "CalConnect"
+          end
+        end
+      end
+
+      def title_validate(root)
+        nil
+      end
+
+      def document(node)
+        result = ["<?xml version='1.0' encoding='UTF-8'?>\n<csd-standard>"]
+        @@draft = node.attributes.has_key?("draft")
+        result << noko { |ixml| front node, ixml }
+        result << noko { |ixml| middle node, ixml }
+        result << "</csd-standard>"
+        result = textcleanup(result.flatten * "\n")
+        ret1 = cleanup(Nokogiri::XML(result))
+        ret1.root.add_namespace(nil, "http://riboseinc.com/isoxml")
+        validate(ret1)
+        ret1.to_xml(indent: 2)
+      end
+
+
     end
   end
 end

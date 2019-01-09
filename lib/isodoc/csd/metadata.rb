8689b2497dc708dc82a2aa18215ce14eedae3489
Ronald Tse <ronald.tse@ribose.com>
2019-01-10 01:23:59 +0800

    Make draft message disappear for published documents

--- lib/isodoc/csd/metadata.rb
+++ lib/isodoc/csd/metadata.rb
@@ -73,7 +73,7 @@ module IsoDoc
       end
 
       def unpublished(status)
-        %w(published withdrawn).include? status.downcase
+        !%w(published withdrawn).include? status.downcase
       end
     end
   end

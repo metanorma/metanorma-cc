6777ac26d90dfd269d2c7c74179bfad50b37726e
Nick Nicholas <opoudjis@gmail.com>
2020-07-05 17:03:08 +1000

    section labelling: https://github.com/metanorma/metanorma-standoc/issues/285

--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -36,8 +36,9 @@ RSpec.describe Metanorma::CC::Processor do
     processor.output(<<~"INPUT", "test.xml", "test.html", :html)
                <csd-standard xmlns="http://riboseinc.com/isoxml">
        <sections>
-       <terms id="H" obligation="normative"><title>Terms, Definitions, Symbols and Abbreviated Terms</title>
+       <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
          <term id="J">
+         <name>1.1.</name>
          <preferred>Term2</preferred>
        </term>
         </terms>

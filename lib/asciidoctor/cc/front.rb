28c3fb177156763294ccbabc21492205a6e7525b
Nick Nicholas <opoudjis@gmail.com>
2020-08-15 22:31:35 +1000

    normalise doctype: https://github.com/metanorma/mn-samples-mpfa/issues/44

--- lib/asciidoctor/cc/front.rb
+++ lib/asciidoctor/cc/front.rb
@@ -33,7 +33,7 @@ module Asciidoctor
 
       def prefix_id(node)
         prefix = "CC"
-        typesuffix = ::Metanorma::CC::DOCSUFFIX[node.attr("doctype")] || ""
+        typesuffix = ::Metanorma::CC::DOCSUFFIX[doctype(node)] || ""
         prefix += "/#{typesuffix}" unless typesuffix.empty?
         status = ::Metanorma::CC::DOCSTATUS[node.attr("status")] || ""
         prefix += "/#{status}" unless status.empty?
@@ -50,11 +50,14 @@ module Asciidoctor
         xml.docnumber node.attr("docnumber")
       end
 
+      @log_doctype = false
+
       def doctype(node)
-        d = node.attr("doctype")
-        unless ::Metanorma::CC::DOCSUFFIX.keys.include?(d)
+        d = super
+        unless ::Metanorma::CC::DOCSUFFIX.keys.include?(d) && !@log_doctype
           @log.add("Document Attributes", nil,
                    "#{d} is not a legal document type: reverting to 'standard'")
+          @log_doctype = true
           d = "standard"
         end
         d

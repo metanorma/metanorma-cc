b958fa7253a7cc84ada25b5d4fd42a5529419edf
Nick Nicholas <opoudjis@gmail.com>
2020-09-17 22:09:45 +1000

    versioning: https://github.com/metanorma/metanorma-standoc/issues/325

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,7 +90,7 @@ END
 
 BLANK_HDR = <<~"HDR"
        <?xml version="1.0" encoding="UTF-8"?>
-       <csd-standard xmlns="https://www.metanorma.org/ns/csd">
+       <csd-standard xmlns="https://www.metanorma.org/ns/csd" type="semantic" version="#{Metanorma::CC::VERSION}">
        <bibdata type="standard">
         <title language="en" format="text/plain">Document title</title>
          <docidentifier type="CalConnect">CC ???</docidentifier>

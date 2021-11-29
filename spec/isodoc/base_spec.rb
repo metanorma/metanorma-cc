f082f5a4f0b2247c844afbb3a5c1100fc239d4ba
Nick Nicholas <opoudjis@gmail.com>
2021-11-30 01:18:41 +1100

    version bumpvi

--- spec/isodoc/base_spec.rb
+++ spec/isodoc/base_spec.rb
@@ -70,47 +70,48 @@ RSpec.describe Asciidoctor::CC do
       <sections/>
       </csd-standard>
     INPUT
-    expect(htmlencode(metadata(csdc.info(docxml, nil)).to_s.gsub(/, :/, ",\n:"))).to be_equivalent_to <<~"OUTPUT"
-      {:accesseddate=>"XXX",
-      :agency=>"CalConnect",
-      :authors=>["Fred Flintstone", "Barney Rubble"],
-      :authors_affiliations=>{""=>["Fred Flintstone"], "Bedrock Inc."=>["Barney Rubble"]},
-      :circulateddate=>"XXX",
-      :confirmeddate=>"XXX",
-      :copieddate=>"XXX",
-      :createddate=>"XXX",
-      :docnumber=>"CC/WD 1000:2001",
-      :docnumeric=>"1000",
-      :doctitle=>"Main Title",
-      :doctype=>"Standard",
-      :doctype_display=>"Standard",
-      :docyear=>"2001",
-      :draft=>"3.4",
-      :draftinfo=>" (draft 3.4, 2000-01-01)",
-      :edition=>"2",
-      :implementeddate=>"XXX",
-      :issueddate=>"XXX",
-      :lang=>"en",
-      :metadata_extensions=>{"doctype"=>"standard", "editorialgroup"=>{"committee_type"=>"A", "committee"=>"TC"}},
-      :obsoleteddate=>"XXX",
-      :publisheddate=>"XXX",
-      :publisher=>"CalConnect",
-      :receiveddate=>"XXX",
-      :revdate=>"2000-01-01",
-      :revdate_monthyear=>"January 2000",
-      :roles_authors_affiliations=>{"author"=>{"Bedrock Inc."=>["Barney Rubble"]}, "editor"=>{""=>["Fred Flintstone"]}},
-      :script=>"Latn",
-      :stage=>"Working Draft",
-      :stage_display=>"Working Draft",
-      :stageabbr=>"WD",
-      :tc=>"TC",
-      :transmitteddate=>"XXX",
-      :unchangeddate=>"XXX",
-      :unpublished=>true,
-      :updateddate=>"XXX",
-      :vote_endeddate=>"XXX",
-      :vote_starteddate=>"XXX"}
-    OUTPUT
+    expect(htmlencode(metadata(csdc.info(docxml, nil)).to_s
+      .gsub(/, :/, ",\n:"))).to be_equivalent_to <<~"OUTPUT"
+        {:accesseddate=>"XXX",
+        :agency=>"CalConnect",
+        :authors=>["Fred Flintstone", "Barney Rubble"],
+        :authors_affiliations=>{""=>["Fred Flintstone"], "Bedrock Inc."=>["Barney Rubble"]},
+        :circulateddate=>"XXX",
+        :confirmeddate=>"XXX",
+        :copieddate=>"XXX",
+        :createddate=>"XXX",
+        :docnumber=>"CC/WD 1000:2001",
+        :docnumeric=>"1000",
+        :doctitle=>"Main Title",
+        :doctype=>"Standard",
+        :doctype_display=>"Standard",
+        :docyear=>"2001",
+        :draft=>"3.4",
+        :draftinfo=>" (draft 3.4, 2000-01-01)",
+        :edition=>"2",
+        :implementeddate=>"XXX",
+        :issueddate=>"XXX",
+        :lang=>"en",
+        :metadata_extensions=>{"doctype"=>"standard", "editorialgroup"=>{"committee_type"=>"A", "committee"=>"TC"}},
+        :obsoleteddate=>"XXX",
+        :publisheddate=>"XXX",
+        :publisher=>"CalConnect",
+        :receiveddate=>"XXX",
+        :revdate=>"2000-01-01",
+        :revdate_monthyear=>"January 2000",
+        :roles_authors_affiliations=>{"author"=>{"Bedrock Inc."=>["Barney Rubble"]}, "editor"=>{""=>["Fred Flintstone"]}},
+        :script=>"Latn",
+        :stage=>"Working Draft",
+        :stage_display=>"Working Draft",
+        :stageabbr=>"WD",
+        :tc=>"TC",
+        :transmitteddate=>"XXX",
+        :unchangeddate=>"XXX",
+        :unpublished=>true,
+        :updateddate=>"XXX",
+        :vote_endeddate=>"XXX",
+        :vote_starteddate=>"XXX"}
+      OUTPUT
   end
 
   it "processes pre" do
@@ -170,7 +171,7 @@ RSpec.describe Asciidoctor::CC do
           <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
             <term id="J">
               <name>1.1.</name>
-              <preferred>Term2</preferred>
+              <preferred><expression><name>Term2</name></expression></preferred>
             </term>
           </terms>
         </sections>
@@ -185,7 +186,7 @@ RSpec.describe Asciidoctor::CC do
             <div id="H">
               <h1>1.&#160; Terms, Definitions, Symbols and Abbreviated Terms</h1>
               <p class="TermNum" id="J">1.1.</p>
-              <p class="Terms" style="text-align:left;">Term2</p>
+              <p class="Terms" style="text-align:left;"><b>Term2</b></p>
             </div>
           </div>
         </body>
@@ -216,7 +217,8 @@ RSpec.describe Asciidoctor::CC do
       </html>
     INPUT
     expect(xmlpp(IsoDoc::CC::HtmlConvert.new({})
-      .cleanup(Nokogiri::XML(input)).to_s)).to be_equivalent_to xmlpp(<<~"OUTPUT")
+      .cleanup(Nokogiri::XML(input)).to_s))
+      .to be_equivalent_to xmlpp(<<~"OUTPUT")
         <?xml version="1.0"?>
         <html>
           <body lang="EN-US" link="blue" vlink="#954F72" xml:lang="EN-US" class="container">
@@ -232,7 +234,7 @@ RSpec.describe Asciidoctor::CC do
               <p class="zzSTDTitle1"/>
               <div id="H"><h1>1.&#xA0; Terms and definitions</h1>
                 <p>For the purposes of this document, the following terms and definitions apply.</p>
-                <p class="TermNum" id="J">1.1.&#xA0;<p class="Terms" style="text-align:left;">Term2</p></p>
+                <p class='Terms' style='text-align:left;' id='J'><strong>1.1.</strong>&#xA0;Term2</p>
               </div>
             </div>
           </body>
@@ -318,76 +320,76 @@ RSpec.describe Asciidoctor::CC do
       .convert("test", input, true)
       .gsub(%r{^.*<body}m, "<body")
       .gsub(%r{</body>.*$}m, "</body>"))).to be_equivalent_to xmlpp(<<~"OUTPUT")
-           <csd-standard xmlns="http://riboseinc.com/isoxml" type="presentation">
-         <preface>
-           <foreword obligation="informative" displayorder="1">
-             <title>Foreword</title>
-             <p id="A">This is a preamble</p>
-           </foreword>
-           <introduction id="B" obligation="informative" displayorder="2">
-             <title>Introduction</title>
-             <clause id="C" inline-header="false" obligation="informative">
-               <title depth="2">Introduction Subsection</title>
-             </clause>
-           </introduction>
-         </preface>
-         <sections>
-           <clause id="D" obligation="normative" displayorder="6">
-             <title depth="1">4.<tab/>Scope</title>
-             <p id="E">Text</p>
-           </clause>
-           <clause id="H" obligation="normative" displayorder="4">
-             <title depth="1">2.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
-             <terms id="I" obligation="normative">
-               <title depth="2">2.1.<tab/>Normal Terms</title>
-               <term id="J"><name>2.1.1.</name>
-                 <preferred>Term2</preferred>
-               </term>
-             </terms>
-             <definitions id="K"><title>2.2.</title>
-               <dl>
-                 <dt>Symbol</dt>
-                 <dd>Definition</dd>
-               </dl>
-             </definitions>
-           </clause>
-           <definitions id="L" displayorder="5"><title>3.</title>
-             <dl>
-               <dt>Symbol</dt>
-               <dd>Definition</dd>
-             </dl>
-           </definitions>
-           <clause id="M" inline-header="false" obligation="normative" displayorder="7">
-             <title depth="1">5.<tab/>Clause 4</title>
-             <clause id="N" inline-header="false" obligation="normative">
-               <title depth="2">5.1.<tab/>Introduction</title>
-             </clause>
-             <clause id="O" inline-header="false" obligation="normative">
-               <title depth="2">5.2.<tab/>Clause 4.2</title>
-             </clause>
-           </clause>
-         </sections>
-         <annex id="P" inline-header="false" obligation="normative" displayorder="8">
-           <title><strong>Appendix A</strong><br/>(normative)<br/><strong>Annex</strong></title>
-           <clause id="Q" inline-header="false" obligation="normative">
-             <title depth="2">A.1.<tab/>Annex A.1</title>
-             <clause id="Q1" inline-header="false" obligation="normative">
-               <title depth="3">A.1.1.<tab/>Annex A.1a</title>
-             </clause>
-           </clause>
-         </annex>
-         <bibliography>
-           <references id="R" normative="true" obligation="informative" displayorder="3">
-             <title depth="1">1.<tab/>Normative References</title>
-           </references>
-           <clause id="S" obligation="informative" displayorder="9">
-             <title depth="1">Bibliography</title>
-             <references id="T" normative="false" obligation="informative">
-               <title depth="2">Bibliography Subsection</title>
-             </references>
-           </clause>
-         </bibliography>
-       </csd-standard>
+            <csd-standard xmlns="http://riboseinc.com/isoxml" type="presentation">
+          <preface>
+            <foreword obligation="informative" displayorder="1">
+              <title>Foreword</title>
+              <p id="A">This is a preamble</p>
+            </foreword>
+            <introduction id="B" obligation="informative" displayorder="2">
+              <title>Introduction</title>
+              <clause id="C" inline-header="false" obligation="informative">
+                <title depth="2">Introduction Subsection</title>
+              </clause>
+            </introduction>
+          </preface>
+          <sections>
+            <clause id="D" obligation="normative" displayorder="6">
+              <title depth="1">4.<tab/>Scope</title>
+              <p id="E">Text</p>
+            </clause>
+            <clause id="H" obligation="normative" displayorder="4">
+              <title depth="1">2.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
+              <terms id="I" obligation="normative">
+                <title depth="2">2.1.<tab/>Normal Terms</title>
+                <term id="J"><name>2.1.1.</name>
+                  <preferred>Term2</preferred>
+                </term>
+              </terms>
+              <definitions id="K"><title>2.2.</title>
+                <dl>
+                  <dt>Symbol</dt>
+                  <dd>Definition</dd>
+                </dl>
+              </definitions>
+            </clause>
+            <definitions id="L" displayorder="5"><title>3.</title>
+              <dl>
+                <dt>Symbol</dt>
+                <dd>Definition</dd>
+              </dl>
+            </definitions>
+            <clause id="M" inline-header="false" obligation="normative" displayorder="7">
+              <title depth="1">5.<tab/>Clause 4</title>
+              <clause id="N" inline-header="false" obligation="normative">
+                <title depth="2">5.1.<tab/>Introduction</title>
+              </clause>
+              <clause id="O" inline-header="false" obligation="normative">
+                <title depth="2">5.2.<tab/>Clause 4.2</title>
+              </clause>
+            </clause>
+          </sections>
+          <annex id="P" inline-header="false" obligation="normative" displayorder="8">
+            <title><strong>Appendix A</strong><br/>(normative)<br/><strong>Annex</strong></title>
+            <clause id="Q" inline-header="false" obligation="normative">
+              <title depth="2">A.1.<tab/>Annex A.1</title>
+              <clause id="Q1" inline-header="false" obligation="normative">
+                <title depth="3">A.1.1.<tab/>Annex A.1a</title>
+              </clause>
+            </clause>
+          </annex>
+          <bibliography>
+            <references id="R" normative="true" obligation="informative" displayorder="3">
+              <title depth="1">1.<tab/>Normative References</title>
+            </references>
+            <clause id="S" obligation="informative" displayorder="9">
+              <title depth="1">Bibliography</title>
+              <references id="T" normative="false" obligation="informative">
+                <title depth="2">Bibliography Subsection</title>
+              </references>
+            </clause>
+          </bibliography>
+        </csd-standard>
       OUTPUT
   end
 
